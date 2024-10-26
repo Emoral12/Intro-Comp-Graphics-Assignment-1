be7e5a7e7e224b8e21a52b26efd5f7af945f01c6
Emoral12 <eugbermorales77@gmail.com>
2024-10-26 16:32:45 -0400

    Main Commit
    Added the files

---
+++ Assets/Scripts/Enemy_Targeting.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_Targeting : MonoBehaviour
+{
+    public GameObject Target;
+    private NavMeshAgent agent;
+    bool isWalking = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalking)
+        {
+            agent.destination = Target.transform.position;
+        }
+        else
+        {
+            agent.destination = transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            isWalking = false;        }
+    }
+
+}
